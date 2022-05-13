aa50537e9acdb458ab14b1ea8ad37ff81efdc48a
Rui Apóstolo <ruiapostolo@gmail.com>
2022-05-13 16:35:43 +0100

    Revision to code. Used named units for file read/write. Added file closing.

--- surface.f90
+++ surface.f90
@@ -11,6 +11,7 @@ do frame = 1, nframecalc
     call readcoordinates
     call heightdistribution
 enddo
+close(trajf)
 call distribution_output
 
 contains
@@ -24,6 +25,15 @@ if (len_trim(inputfile) == 0) then
 endif
 
 call readinput
+
+! open lammps trajectory file
+open(trajf, file = trajfile, status='old')
+
+! get ntotal
+call readheader
+! rewind trajectory file
+rewind(trajf)
+call init_hdist
 ! allocate arrays
 allocate(nindex(ntotal))
 allocate(atomtype(ntotal))
@@ -32,9 +42,6 @@ allocate(y(ntotal))
 allocate(z(ntotal))
 
 call readmasses
-
-! open lammps trajectory file
-open(11, file = trajfile, status='old')
 
 call skipframe
 
@@ -46,21 +53,22 @@ end subroutine start
 
 ! reads analysis.input input file
 subroutine readinput
-open(10, file = inputfile, status='old')
-read(10, *)
-read(10, *)
-read(10, '(a)') trajfile
-read(10, *) outputfile
-read(10, *) massfile
-read(10, *) nframe
-read(10, *) nskip
-read(10, *) ntotal
-read(10, *) ntypes
-read(10, *) binwidth
-read(10, *) axisid
+open(inputf, file = inputfile, status='old')
+read(inputf, *)
+read(inputf, *)
+read(inputf, '(a)') trajfile
+read(inputf, *) outputfile
+read(inputf, *) massfile
+read(inputf, *) nframe
+read(inputf, *) nskip
+read(inputf, *) ntotal
+read(inputf, *) ntypes
+read(inputf, *) binwidth
+read(inputf, *) axisid
 write(*,*) trajfile
+close(inputf)
 
-if (axisid .gt. 3) then
+if ((axisid .gt. 3) .or. (axisid .lt. 0)) then
   write(*,*) 'Axis id must take the value 1(x), 2(y) or 3(z).'
   call exit
 endif
@@ -75,7 +83,7 @@ end subroutine readinput
 subroutine skipframe
 integer:: i
 do i = 1, nskip*(ntotal+9)
-  read(11, *)
+  read(trajf, *)
 enddo
 write(*,*) "SKIPPED FRAMES : ", nskip
 end subroutine skipframe
@@ -85,33 +93,37 @@ subroutine readmasses
 
   allocate(mass(ntypes))
 
-  open(12, file = massfile, status='old')
-  read(12, *)
-  read(12, *)
+  open(massf, file = massfile, status='old')
+  read(massf, *)
+  read(massf, *)
   do i = 1, ntypes
-    read(12, *) j, mass(i)
+    read(massf, *) j, mass(i)
     write(*,*) j, mass(i)
   enddo
+  close(massf)
 end subroutine readmasses
 
 ! reads in box length, timestep and natoms from the LAMMPS trajectory headers
 subroutine readheader
-read(11, *) headertext(1)   
-read(11, *) timestep
-read(11, *) headertext(2)
-read(11, *) natom
-read(11, *) headertext(3)
-read(11, *) xmin, xmax
-read(11, *) ymin, ymax
-read(11, *) zmin, zmax
-read(11, *) headertext(4)
+read(trajf, *) headertext(1)   
+read(trajf, *) timestep
+read(trajf, *) headertext(2)
+read(trajf, *) natom
+read(trajf, *) headertext(3)
+read(trajf, *) xmin, xmax
+read(trajf, *) ymin, ymax
+read(trajf, *) zmin, zmax
+read(trajf, *) headertext(4)
 
 Lx = xmax-xmin
 Ly = ymax-ymin
 Lz = zmax-zmin
 
-!if(natom  .ne. ntotal) stop 'mismatched number of atoms'
-if (first_read) then
+end subroutine readheader
+
+! initialize hdist
+subroutine init_hdist
+
   if (axisid .eq. 1) then
     nbins = int((xmax-xmin)/binwidth)+1
   else if (axisid .eq. 2) then
@@ -121,9 +133,8 @@ if (first_read) then
   endif
   allocate(hdist(ntypes, nbins))
   hdist = 0.0
-  first_read = .FALSE.
-endif
-end subroutine readheader
+
+end subroutine init_hdist
 
 ! reads in the atom types and coordinates from the LAMMPS trajectory file
 subroutine readcoordinates
@@ -138,48 +149,57 @@ subroutine readcoordinates
 end subroutine readcoordinates
 
 subroutine heightdistribution
-integer(8):: i, hbin
-if (axisid .eq. 1) then
-  do i = 1, ntotal
-    hbin = int(x(i)/binwidth+1.0)
-    hdist(atomtype(i), hbin) = hdist(atomtype(i), hbin) + 1.0
-  enddo
-else if (axisid .eq. 2) then
-  do i = 1, ntotal
-    hbin = int(y(i)/binwidth+1.0)
-    hdist(atomtype(i), hbin) = hdist(atomtype(i), hbin) + 1.0
-  enddo
-else if (axisid .eq. 3) then
-  do i = 1, ntotal
-    hbin = int(z(i)/binwidth+1.0)
-    hdist(atomtype(i), hbin) = hdist(atomtype(i), hbin) + 1.0
-  enddo
-endif
-hdistnrm = hdistnrm+1.0
+  integer(8):: i, hbin
+  ! transform these into a function, and call the function.
+  if (axisid .eq. 1) then
+    do i = 1, ntotal
+      hbin = int(x(i)/binwidth+1.0)
+      hdist(atomtype(i), hbin) = hdist(atomtype(i), hbin) + 1.0
+    enddo
+  else if (axisid .eq. 2) then
+    do i = 1, ntotal
+      hbin = int(y(i)/binwidth+1.0)
+      hdist(atomtype(i), hbin) = hdist(atomtype(i), hbin) + 1.0
+    enddo
+  else if (axisid .eq. 3) then
+    do i = 1, ntotal
+      hbin = int(z(i)/binwidth+1.0)
+      hdist(atomtype(i), hbin) = hdist(atomtype(i), hbin) + 1.0
+    enddo
+  endif
+
+  hdistnrm = hdistnrm+1.0
 end subroutine heightdistribution
 
 subroutine distribution_output
   integer(8):: i, j
   real(dp):: binvolume
-  open(unit = 101, file='conc.dat', status='unknown')
-  open(unit = 102, file='density.dat', status='unknown')
+  open(unit=concf, file='conc.dat', status='unknown')
+  open(unit=densf, file='density.dat', status='unknown')
+  ! transform these into a function, and call the function.
   if (axisid .eq. 1) then
     binvolume = Ly*Lz*binwidth
-    write(101, *) '#  x', ('    ', j, j = 1, ntypes)
-    write(102, *) '#  x', ('    ', j, j = 1, ntypes)
+    write(concf, *) '#  x', ('    ', j, j = 1, ntypes)
+    write(densf, *) '#  x', ('    ', j, j = 1, ntypes)
   else if (axisid .eq. 2) then
     binvolume = Lx*Lz*binwidth
-    write(101, *) '#  y', ('    ', j, j = 1, ntypes)
-    write(102, *) '#  y', ('    ', j, j = 1, ntypes)
+    write(concf, *) '#  y', ('    ', j, j = 1, ntypes)
+    write(densf, *) '#  y', ('    ', j, j = 1, ntypes)
   else if (axisid .eq. 3) then
     binvolume = Lx*Ly*binwidth
-    write(101, *) '#  z', ('    ', j, j = 1, ntypes)
-    write(102, *) '#  z', ('    ', j, j = 1, ntypes)
+    write(concf, *) '#  z', ('    ', j, j = 1, ntypes)
+    write(densf, *) '#  z', ('    ', j, j = 1, ntypes)
   endif
+
   do i = 1, nbins
-    write(101, *) (binwidth*(dble(i)-0.5)), (hdist(j, i)/binvolume/hdistnrm, j = 1, ntypes)
-    write(102, *) (binwidth*(dble(i)-0.5)), (hdist(j, i)*mass(j)/binvolume/hdistnrm, j = 1, ntypes)
+    write(concf, *) (binwidth*(dble(i)-0.5)), &
+      (hdist(j, i)/binvolume/hdistnrm, j = 1, ntypes)
+    write(densf, *) (binwidth*(dble(i)-0.5)), &
+      (hdist(j, i)*mass(j)/binvolume/hdistnrm, j = 1, ntypes)
   enddo
+
+  close(concf)
+  close(densf)
 end subroutine distribution_output
 
 end program surface
