d73a01459652f5fb0dede58ba44f78e31bd3ed0a
JColdstream <jonathancoldstream@gmail.com>
2023-01-23 16:33:14 +0000

    Added option for coordinates to be calculated in terms of fractional box lengths.

--- surface.f90
+++ surface.f90
@@ -6,7 +6,7 @@ integer(8):: frame
 
 call start
 do frame = 1, nframecalc
-  write(*,*) frame
+  if (mod(frame, 50) .eq. 0) write(*,*) frame
     call readheader
     call readcoordinates
     call heightdistribution
@@ -42,7 +42,7 @@ allocate(y(ntotal))
 allocate(z(ntotal))
 
 call readmasses
-
+call readscatteringlengths
 call skipframe
 
 ! initialise counter
@@ -57,13 +57,14 @@ open(inputf, file = inputfile, status='old')
 read(inputf, *)
 read(inputf, *)
 read(inputf, '(a)') trajfile
-read(inputf, *) outputfile
 read(inputf, *) massfile
+read(inputf, *) scatteringfile
 read(inputf, *) nframe
 read(inputf, *) nskip
 read(inputf, *) ntypes
 read(inputf, *) binwidth
 read(inputf, *) axisid
+read(inputf, *) fractional_coords
 write(*,*) trajfile
 close(inputf)
 
@@ -72,11 +73,42 @@ if ((axisid .gt. 3) .or. (axisid .lt. 0)) then
   call exit
 endif
 
+if (fractional_coords .eq. 0) then
+  fractional_bool = .FALSE.
+else if (fractional_coords .eq. 1) then
+  fractional_bool = .TRUE.
+else
+  write(*,*) 'Fractional coordinates must take the value of 0 (absolute) or 1 (fractional).'
+  call exit
+end if  
+
 ! calculate relevant quantities for later
 ! nanalyse = nmol*molsize
 nframecalc = nframe-nskip
 
 end subroutine readinput
+
+subroutine readscatteringlengths
+  integer(sp):: i, j
+
+  allocate(b(ntypes))
+  ! reads scattering lengths input
+  if (scatteringfile .eq. '0') then
+    do i = 1, ntypes
+      b(i) = 1.0_dp
+    enddo
+  else
+    open(12, file = scatteringfile, status='old')
+    read(12, *)
+    read(12, *)
+    do i = 1, ntypes
+      read(12, *) j, b(j)
+    enddo
+    do i = 1, ntypes
+      b(i) = b(i) * 0.00001_dp
+    enddo
+  endif
+end subroutine readscatteringlengths
 
 ! skips frames that don't need to be analysed
 subroutine skipframe
@@ -97,9 +129,10 @@ subroutine readmasses
   read(massf, *)
   do i = 1, ntypes
     read(massf, *) j, mass(i)
-    write(*,*) j, mass(i)
+    ! write(*,*) j, mass(i)
   enddo
   close(massf)
+  write(*,*) 'Masses read.'
 end subroutine readmasses
 
 ! reads in box length, timestep and natoms from the LAMMPS trajectory headers
@@ -122,14 +155,17 @@ end subroutine readheader
 
 ! initialize hdist
 subroutine init_hdist
-
-  if (axisid .eq. 1) then
-    nbins = int((xmax-xmin)/binwidth)+1
-  else if (axisid .eq. 2) then
-    nbins = int((ymax-ymin)/binwidth)+1
-  else if (axisid .eq. 3) then
-    nbins = int((zmax-zmin)/binwidth)+1
-  endif
+  if (fractional_bool) then
+    nbins = int(1/binwidth) + 1
+  else
+    if (axisid .eq. 1) then
+      nbins = int((xmax-xmin)/binwidth)+1
+    else if (axisid .eq. 2) then
+      nbins = int((ymax-ymin)/binwidth)+1
+    else if (axisid .eq. 3) then
+      nbins = int((zmax-zmin)/binwidth)+1
+    endif
+  end if
   allocate(hdist(ntypes, nbins))
   hdist = 0.0
 
@@ -145,6 +181,11 @@ subroutine readcoordinates
  x = x-xmin
  y = y-ymin
  z = z-zmin
+ if (fractional_bool .eqv. .TRUE.) then
+   x = x / (xmax-xmin)   
+   y = y / (ymax-ymin)   
+   z = z / (zmax-zmin)   
+ end if  
 end subroutine readcoordinates
 
 subroutine heightdistribution
@@ -173,8 +214,9 @@ end subroutine heightdistribution
 subroutine distribution_output
   integer(8):: i, j
   real(dp):: binvolume
-  open(unit=concf, file='conc.dat', status='unknown')
-  open(unit=densf, file='density.dat', status='unknown')
+  open(unit = concf, file='conc.dat', status='unknown')
+  open(unit = densf, file='density.dat', status='unknown')
+  open(unit = sldf, file='sld.dat', status='unknown')
   ! transform these into a function, and call the function.
   if (axisid .eq. 1) then
     binvolume = Ly*Lz*binwidth
@@ -191,14 +233,17 @@ subroutine distribution_output
   endif
 
   do i = 1, nbins
-    write(concf, *) (binwidth*(dble(i)-0.5)), &
+    write(concf, '(f10.3, 999(e15.6))') (binwidth*(dble(i)-0.5)), &
       (hdist(j, i)/binvolume/hdistnrm, j = 1, ntypes)
-    write(densf, *) (binwidth*(dble(i)-0.5)), &
+    write(densf, '(f10.3, 999(e15.6))') (binwidth*(dble(i)-0.5)), &
       (hdist(j, i)*mass(j)/binvolume/hdistnrm, j = 1, ntypes)
+    write(sldf, '(f10.3, 999(e15.6))') (binwidth*(dble(i)-0.5)), &
+      (hdist(j, i)*b(j)/binvolume/hdistnrm, j = 1, ntypes)
   enddo
 
   close(concf)
   close(densf)
+  close(sldf)
 end subroutine distribution_output
 
 end program surface
